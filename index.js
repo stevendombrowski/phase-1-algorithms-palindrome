5d9afaa97952c4dbd8958fd6f0a9ad278e90b675
Steven Dombrowski <dombrowskis153@yahoo.com>
2021-12-11 02:56:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length -1 - i;
+    if (word[i] !== word[j]) return false;
+  }
+  return true;
 }
-
 /* 
   Add your pseudocode here
 */
